d8de247f3dae4cb405fe6babc9d1f8932b2df511
Rafael Garcia <rafbgarcia@gmail.com>
2022-05-25 20:17:15 -0300

    Properly implement Nav

--- o2_web/src/pages/Home.tsx
+++ o2_web/src/pages/Home.tsx
@@ -1,24 +1,3 @@
-import Dashboard from '../components/Dashboard'
-import * as o2 from '../lib/o2'
-
-const theme: DashboardTheme = {
-  // title: ({ children }) => <Title>{children}</Title>,
-  // gridRow: ({ children }) => <section className="widget-group">{children}</section>,
-  // lastUpdateAt: () => null,
-  widgets: {
-    pivot: {
-      // table: ({ children }: any) => <Table size="sm">{children}</Table>,
-    },
-  },
-  // filters: {
-  //   // ...
-  // }
-}
-
 export const Home = () => {
-  const { data, error } = o2.getDashboard(1)
-
-  if (error) return <p>Error: {error}</p>
-
-  return <Dashboard data={data} theme={theme} />
+  return <h3>Welcome home</h3>
 }
--- o2_web/src/pages/Layout.tsx
+++ o2_web/src/pages/Layout.tsx
@@ -1,7 +1,14 @@
+import { Fragment } from 'react'
 import { Link, LinkProps } from 'react-router-dom'
 import { Outlet, useMatch, useResolvedPath } from 'react-router-dom'
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
 import classnames from 'classnames'
 import logo from '../assets/logo.png'
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
 
 const ActiveLink = ({ children, to, ...props }: LinkProps) => {
   let resolved = useResolvedPath(to)
@@ -18,15 +25,21 @@ const ActiveLink = ({ children, to, ...props }: LinkProps) => {
   )
 }
 
+const navigation = [
+  <ActiveLink to="/">Home</ActiveLink>,
+  <ActiveLink to="/datasets">Datasets</ActiveLink>,
+  <ActiveLink to="/dashboards">Dashboards</ActiveLink>,
+]
+
 const Nav = () => {
   return (
-    <nav className="bg-gray-800">
+    <Disclosure as="nav" className="bg-gray-800">
       <div className="px-4">
         <div className="relative flex items-center justify-between h-16">
           <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
             <div className="flex-shrink-0 flex items-center">
               <img className="block h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" />
-              <img className="block h-8 w-auto ml-2" src={logo} />
+              <img className="block h-7 w-auto ml-2" src={logo} />
             </div>
             <div className="hidden sm:block sm:ml-6">
               <div className="flex space-x-4">
@@ -42,57 +55,68 @@ const Nav = () => {
               className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
             >
               <span className="sr-only">View notifications</span>
-              <svg
-                className="h-6 w-6"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="2"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-                />
-              </svg>
+              <BellIcon className="h-6 w-6" aria-hidden="true" />
             </button>
 
-            <div className="ml-3 relative">
+            {/* Profile dropdown */}
+            <Menu as="div" className="ml-3 relative">
               <div>
-                <button
-                  type="button"
-                  className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-                  id="user-menu-button"
-                  aria-expanded="false"
-                  aria-haspopup="true"
-                >
-                  <img className="h-8 w-8 rounded-full" src="https://avatars.githubusercontent.com/u/1904314?v=4" />
-                </button>
+                <Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                  <span className="sr-only">Open user menu</span>
+                  <img
+                    className="h-8 w-8 rounded-full"
+                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                    alt=""
+                  />
+                </Menu.Button>
               </div>
-
-              <div
-                className="hidden origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
-                role="menu"
-                aria-orientation="vertical"
-                aria-labelledby="user-menu-button"
+              <Transition
+                as={Fragment}
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
               >
-                <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" id="user-menu-item-0">
-                  Your Profile
-                </a>
-                <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" id="user-menu-item-1">
-                  Settings
-                </a>
-                <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" id="user-menu-item-2">
-                  Sign out
-                </a>
-              </div>
-            </div>
+                <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                  <Menu.Item>
+                    {({ active }) => (
+                      <a
+                        href="#"
+                        className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                      >
+                        Your Profile
+                      </a>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <a
+                        href="#"
+                        className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                      >
+                        Settings
+                      </a>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <a
+                        href="#"
+                        className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                      >
+                        Sign out
+                      </a>
+                    )}
+                  </Menu.Item>
+                </Menu.Items>
+              </Transition>
+            </Menu>
           </div>
         </div>
       </div>
-    </nav>
+    </Disclosure>
   )
 }
 
@@ -100,7 +124,9 @@ export const Layout = () => {
   return (
     <>
       <Nav />
-      <Outlet />
+      <main>
+        <Outlet />
+      </main>
     </>
   )
 }
