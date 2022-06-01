247ef5cbf63cba35e04ef16f1a57df38ae4a0e00
Rafael Garcia <rafbgarcia@gmail.com>
2022-06-01 14:22:36 -0300

    Update CORS

--- powerBi/settings.py
+++ powerBi/settings.py
@@ -131,7 +131,7 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
+    "http://localhost:4000",
 ]
 
 # https://stackoverflow.com/a/70833150/1488741
