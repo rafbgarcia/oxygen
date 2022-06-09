01db800946033149242f97b253b0c135f15873e1
Rafael Garcia <rafbgarcia@gmail.com>
2022-06-08 22:58:11 -0300

    Pivot loading skeleton

--- o2_web/src/components/Widget/Pivot.tsx
+++ o2_web/src/components/Widget/Pivot.tsx
@@ -1,5 +1,6 @@
 import { Table } from "playbook-ui"
 import { useWidgetQuery } from "../../lib/codegenGraphql"
+import { times } from "lodash-es"
 import "./Pivot.css"
 
 const tableHtmlComponent = (content: string) => {
@@ -9,7 +10,7 @@ const tableHtmlComponent = (content: string) => {
 }
 
 export const Pivot = ({ widget, dataset }) => {
-  const { data, loading } = useWidgetQuery({
+  const { data, loading, error } = useWidgetQuery({
     variables: {
       type: widget.type,
       build: widget.buildInfo,
@@ -18,10 +19,12 @@ export const Pivot = ({ widget, dataset }) => {
   })
 
   if (loading) {
-    return "Loading..."
+    return <Loading />
+  } else if (!data || error) {
+    return <p>Error {JSON.stringify(error)}</p>
   }
 
-  const table = tableHtmlComponent(data?.widget.html)
+  const table = tableHtmlComponent(data.widget.html)
   const theadHTML = table?.children[0]?.innerHTML
   const tbodyHTML = table?.children[1]?.innerHTML
     .replace(/\<td/g, "<td class='data-cell'")
@@ -32,5 +35,36 @@ export const Pivot = ({ widget, dataset }) => {
       <thead dangerouslySetInnerHTML={{ __html: theadHTML }}></thead>
       <tbody dangerouslySetInnerHTML={{ __html: tbodyHTML }}></tbody>
     </Table>
+  )
+}
+
+const Loading = () => {
+  const cols = 4
+  const rows = 20
+  return (
+    <div className="animate-pulse">
+      <Table>
+        <thead>
+          <tr>
+            {times(cols, () => (
+              <th>
+                <div className="h-4 bg-slate-300 rounded"></div>
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {times(rows, () => (
+            <tr>
+              {times(cols, () => (
+                <td>
+                  <div className="h-8 bg-slate-300 rounded"></div>
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
   )
 }
