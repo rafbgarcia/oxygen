19d42c8bb3dfb96cbda11f0ff05394f9a8b735ea
Rafael Garcia <rafbgarcia@gmail.com>
2022-07-02 21:54:07 -0300

    Rename folder

--- manage/wsgi.py
+++ manage/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for powerBi project.
+WSGI config for manage project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'powerBi.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "manage.settings")
 
 application = get_wsgi_application()
